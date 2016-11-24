686949242a83caff547e0eef5b73f6ceae7aa303
Andrew McConachie <andrew@depht.com>
2016-11-23 21:49:03 -0500

    Started hackin

--- danish.py
+++ danish.py
@@ -1,30 +1,55 @@
 #!/usr/bin/env python
 
-import pcapy as pcap
 import os
+import pcapy as pcap
 import dns
 
 
 # Initializes a pcap capture object
-# Returns a string on failure and ifCap reference on success
+# Returns a string on failure and pcapy.Reader on success
 def initRx(iface, filt):
   if(os.getuid() or os.geteuid()):
     return "Error:Requires root access"
   
   if(not iface in pcap.findalldevs()):
-    return "Error:Bad interface " + self.ifName
+    return "Error:Bad interface " + iface
 
-  ifCap = pcap.open_live(iface, 65536, True, 10)
-  if(ifCap.datalink() != pcap.DLT_EN10MB):
+  pr = pcap.open_live(iface, 65536, True, 10)
+  if(pr.datalink() != pcap.DLT_EN10MB):
     return "Error:Interface not Ethernet " + iface
 
   try:
-    ifCap.setfilter(filt)
+    pr.setfilter(filt)
   except pcap.PcapError:
     return "Error:Bad capture filter"
 
-  return ifCap
+  return pr
+
+# Prints a packet
+def printPkt(hdr, pkt):
+  # Print timestamps  
+  tAbs, tRel = hdr.getts()
+  print "tAbs>" + str(tAbs) + " tRel>" + str(tRel) + " " 
+
+  s = []
+  for c in pkt:
+    s.append(hex(ord(c)).lstrip("0x").zfill(2))
+
+  # Print Linklayer
+  dst = ':'.join(s[:6])
+  src = ':'.join(s[6:12])
+  etype = ':'.join(s[12:14])
+  print "dst>" + dst + " src>" + src + " etype>" + etype
+
+  # Print rest of packet
+  print ':'.join(s[14:]) + "\n"
+
+  
+pr = initRx('br-lan', "icmp")
+while True:
+  pkt = pr.dispatch(1, printPkt)
 
 
-print initRx('br-lan', "udp")
+
+ 
 print "Finished Execution"
