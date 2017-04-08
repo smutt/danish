7d5174306383b21b80c9bf6b54d135eb628c5512
Andrew McConachie <andrew@depht.com>
2017-04-08 18:45:07 -0400

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""A daemon for middlebox DANE(RFC 6698) """
+
+__author__ = 'Andrew McConachie'
+__author_email__ = 'andrew@depht.com'
+__license__ = 'GPL3'
+__url__ = 'https://github.com/smutt/danish'
+__version__ = '0.1'
+
+import danish
