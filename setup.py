d796d22c7fc803a8a7b0d11b6a32a68c8ef6edeb
Andrew McConachie <andrew@depht.com>
2017-04-09 12:05:29 -0400

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='danish',
+      version='0.1',
+      description='Middlebox DANE(RFC 6698) daemon for LEDE/OpenWRT',
+      author='Andrew McConachie',
+      author_email='andrew@depht.com',
+      url='https://github.com/smutt/danish',
+      packages=['danish'],
+     )
