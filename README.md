59dd0afe4e97958b29d9d09bbd5662a3031b6c76
Andrew McConachie <andrew@depht.com>
2017-04-10 16:22:47 -0400

    Quick doc update

---
+++ README.md
@@ -0,0 +1,2 @@
+Danish listens for HTTPS TLS handshake traffic and captures the TLS/SNI and certificates. Then it performs DNS lookups for DNS TLSA records to determine if the responding server is sending the correct X.509 certificate in its TLS ServerHello message. If the certificates and DNS TLSA records do NOT match, iptables ACLs are installed to block user traffic to the offending website. Currently supports TLS 1.0 - 1.2.
+
--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ from setuptools import setup, find_packages
 setup(name='danish',
       version='0.1',
       description='Middlebox DANE(RFC 6698) daemon for LEDE/OpenWRT',
+      long_description='Danish listens for HTTPS TLS handshake traffic and captures the TLS/SNI and certificates. Then it performs DNS lookups for DNS TLSA records to determine if the responding server is sending the correct X.509 certificate in its TLS ServerHello message. If the certificates and DNS TLSA records do NOT match, iptables ACLs are installed to block user traffic to the offending website. Currently supports TLS 1.0 - 1.2.',
       author='Andrew McConachie',
       author_email='andrew@depht.com',
       url='https://github.com/smutt/danish',
+      platform='LEDE','OpenWRT','Linux',
+      license='GPL-3',
       packages=find_packages()
       )
