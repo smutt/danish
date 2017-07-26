c10155416d0e570c5224401ad008ccb9fa6151bd
Andrew McConachie <andrew@depht.com>
2017-07-25 20:00:54 -0400

    Add GPL header

--- danish.py
+++ danish.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+#  The file is part of the Danish Project.
+#
+#  The Danish Project is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  The Danish Project  is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#  Copyright (C) 2017, Andrew McConachie, <andrew@depht.com>
+
 import sys
 import os
 import datetime
